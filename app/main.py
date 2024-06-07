8b4cffb189c7a63c05338d815ec5cb6de57c2342
dohkodesparche <dohkodesparche@gmail.com>
2024-06-07 17:50:35 -0500

    nuevos archivos

--- app/main.py
+++ app/main.py
@@ -14,12 +14,13 @@ def run():
   
   #### Esto es para tomas los datos de una fila
   country = input('Type Country => ')
+  print(country)
   result = utils.population_by_country(data, country)
 
   if len(result) > 0:
     country = result[0]
     labels, values = utils.get_population(country)
-    chars.generate_bar_chart(labels, values)
+    chars.generate_bar_chart(country['Country/Territory'],labels, values)
   
 
 if __name__ == '__main__':
