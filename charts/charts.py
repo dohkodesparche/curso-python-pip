6b45584b3822ce5299153a00fc9fafa50efb2e4d
dohkodesparche <dohkodesparche@gmail.com>
2024-06-07 16:38:45 -0500

    charts1

--- charts/charts.py
+++ charts/charts.py
@@ -2,7 +2,7 @@ import  matplotlib.pyplot as plt
 
 def generate_pie_chart():
     labels=['A','B','C','D']
-    values = [200,40,30]
+    values = [200,40,120]
 
     fig, ax =plt.subplots()
     ax.pie(values, labels=labels)
